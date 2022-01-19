53acc93b869553074b5732441913df512eaa2698
NickPerozzi <53713045+NickPerozzi@users.noreply.github.com>
2022-01-19 11:52:51 -0600

    deleted @GET comments in LyricsApi.kt

--- app/src/main/java/com/perozzi_package/smashmouthsonggenerator/api/LyricsApi.kt
+++ app/src/main/java/com/perozzi_package/smashmouthsonggenerator/api/LyricsApi.kt
@@ -12,19 +12,4 @@ interface LyricsApi {
         @Body albumWeightDict: Map<String, Int>
     ): Response<Lyrics>
 
-
-/*
-    @GET("/test/transactions")
-    suspend fun getLyrics(
-        @Query("fymW") fymW: String,
-        @Query("alW") alW: String,
-        @Query("smW") smW: String,
-        @Query("gtpW") gtpW: String,
-        @Query("asshW") asshW: String,
-        @Query("tgorW") tgorW: String,
-        @Query("sgW") sgW: String,
-        @Query("mW") mW: String,
-    ): Response<Lyrics>
-*/
-
 }
